12f46ab5e8024d0d06f0532cb96120e2d6cfe450
Apocryphon06 <hrithikchandrashekar@gmail.com>
2023-04-01 09:02:23 +0530

    clean up

--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -1,24 +1,21 @@
-import React, { useMemo, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
   const navigate = useNavigate();
-  const [count, setCount] = useState(10);
+  // const [count, setCount] = useState(10);
 
-  useMemo(() => {
-    setInterval(() => {
-      setCount(count - 1);
-      if (count === 0) {
-        return navigate("/");
-      }
-    }, 1000);
-  }, [count]);
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center mt-[45vh]">
       <p className="text-[#363636] text-2xl font-semibold">404 | Not Found</p>
       <p className="text-[#363636] text-base font-light">
-        You will be redirected to home in {count} seconds
+        You will be redirected to home in 5 seconds
       </p>
     </div>
   );
